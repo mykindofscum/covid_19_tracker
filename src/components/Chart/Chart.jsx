da7789e735f550a430ddca57b88ba807f5c86fcf
mykindofscum <alvinkchan99@gmail.com>
2020-08-25 13:17:53 -0700

    Add chart functions with api data

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { line, Bar, Line } from "react-chartjs-2";
+
+import styles from "./Chart.module.css";
 
 const Chart = () => {
-  return <h1>Chart</h1>;
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    fetchAPI();
+  });
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        // map that returns all of the dates
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
 };
 
 export default Chart;
